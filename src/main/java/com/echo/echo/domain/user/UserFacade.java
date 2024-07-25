c22cc9ddb05b01256d03bf666d048d60b86980fb
hyun <hyun1202a@gmail.com>
2024-07-25 18:37:26 +0900

    fix: UserFacade 클래스명 변경

--- src/main/java/com/echo/echo/domain/user/UserFacade.java
+++ src/main/java/com/echo/echo/domain/user/UserFacade.java
@@ -2,14 +2,13 @@ package com.echo.echo.domain.user;
 
 import com.echo.echo.domain.user.dto.UserRequestDto;
 import com.echo.echo.domain.user.dto.UserResponseDto;
-import com.echo.echo.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Component
-public class UserFacadeService {
+public class UserFacade {
     private final UserService userService;
 
     public Mono<UserResponseDto> signup(UserRequestDto userRequestDto) {
