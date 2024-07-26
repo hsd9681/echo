c9e4b0f0be618ff4ca5b7f34537406e097a61d42
kiseokkm <311kskim@gmail.com>
2024-07-27 02:13:00 +0900

    fix: space 에러 응답 처리 변경

--- src/main/java/com/echo/echo/domain/space/SpaceController.java
+++ src/main/java/com/echo/echo/domain/space/SpaceController.java
@@ -6,6 +6,7 @@ import com.echo.echo.domain.space.dto.SpaceRequestDto;
 import com.echo.echo.domain.space.dto.SpaceResponseDto;
 import com.echo.echo.security.principal.UserPrincipal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -24,18 +25,20 @@ public class SpaceController {
     private final SpaceFacade spaceFacade;
 
     @PostMapping
-    public Mono<ResponseEntity<SpaceResponseDto>> createSpace(@RequestBody SpaceRequestDto requestDto) {
+    public Mono<ResponseEntity<SpaceResponseDto>> createSpace(
+        @RequestBody SpaceRequestDto requestDto) {
         return spaceFacade.createSpace(requestDto)
             .map(ResponseEntity::ok);
     }
 
     @PutMapping("/{spaceId}")
-    public Mono<ResponseEntity<String>> updateSpace(@PathVariable Long spaceId, @RequestBody SpaceRequestDto requestDto) {
+    public Mono<ResponseEntity<SpaceResponseDto>> updateSpace(@PathVariable Long spaceId,
+        @RequestBody SpaceRequestDto requestDto) {
         return spaceFacade.updateSpace(spaceId, requestDto)
-            .map(spaceResponse -> ResponseEntity.ok(CommonCode.SUCCESS.getMsg()))
-            .onErrorResume(CustomException.class, e -> Mono.just(ResponseEntity.badRequest().body(e.getBaseCode().getCommonReason().getMsg())));
+            .map(ResponseEntity::ok)
+            .onErrorResume(CustomException.class,
+                e -> Mono.just(ResponseEntity.badRequest().body(null)));
     }
-
 
     @GetMapping("/public")
     public Flux<SpaceResponseDto> getAllPublicSpaces() {
@@ -43,28 +46,29 @@ public class SpaceController {
     }
 
     @GetMapping("/{spaceId}")
-    public Mono<ResponseEntity<String>> getSpaceById(@PathVariable Long spaceId) {
+    public Mono<ResponseEntity<Object>> getSpaceById(@PathVariable Long spaceId) {
         return spaceFacade.getSpaceById(spaceId)
-            .map(spaceResponse -> ResponseEntity.ok(CommonCode.SUCCESS.getMsg()))
-            .onErrorResume(CustomException.class, e -> Mono.just(ResponseEntity.badRequest().body(e.getBaseCode().getCommonReason().getMsg())));
+            .map(spaceResponseDto -> ResponseEntity.ok((Object) spaceResponseDto))
+            .onErrorResume(CustomException.class, e -> Mono.just(
+                ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body((Object) e.getBaseCode().getMsg())));
     }
-
 
     @DeleteMapping("/{spaceId}")
     public Mono<ResponseEntity<String>> deleteSpace(@PathVariable Long spaceId) {
         return spaceFacade.deleteSpace(spaceId)
             .then(Mono.just(ResponseEntity.ok(CommonCode.DELETE_SUCCESS.getMsg())))
-            .onErrorResume(CustomException.class, e -> Mono.just(ResponseEntity.badRequest().body(e.getBaseCode().getCommonReason().getMsg())));
+            .onErrorResume(CustomException.class, e -> Mono.just(
+                ResponseEntity.badRequest().body(e.getBaseCode().getCommonReason().getMsg())));
     }
-
 
     @PostMapping("/join/{uuid}")
-    public Mono<ResponseEntity<String>> joinSpace(@PathVariable String uuid, @AuthenticationPrincipal UserPrincipal userPrincipal) {
+    public Mono<ResponseEntity<Object>> joinSpace(@PathVariable String uuid,
+        @AuthenticationPrincipal UserPrincipal userPrincipal) {
         return spaceFacade.joinSpace(uuid, userPrincipal.getUser().getId())
-            .map(spaceResponse -> ResponseEntity.ok(CommonCode.ENTRY_SUCCESS.getMsg()))
-            .onErrorResume(CustomException.class, e -> Mono.just(ResponseEntity.badRequest().body(e.getBaseCode().getCommonReason().getMsg())));
+            .map(response -> ResponseEntity.ok((Object) response))
+            .onErrorResume(CustomException.class, e -> Mono.just(ResponseEntity.badRequest()
+                .body((Object) e.getBaseCode().getCommonReason().getMsg())));
     }
-
-
 
 }
