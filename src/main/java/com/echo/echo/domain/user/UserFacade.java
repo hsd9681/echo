6a2e215b7b5149096f48579e55da7e71c812fa89
hyun <hyun1202a@gmail.com>
2024-07-26 21:03:14 +0900

    bugfix: 로그인 기능 누락 코드 수정

--- src/main/java/com/echo/echo/domain/user/UserFacade.java
+++ src/main/java/com/echo/echo/domain/user/UserFacade.java
@@ -2,6 +2,7 @@ package com.echo.echo.domain.user;
 
 import com.echo.echo.domain.user.dto.UserRequestDto;
 import com.echo.echo.domain.user.dto.UserResponseDto;
+import com.echo.echo.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -14,5 +15,9 @@ public class UserFacade {
     public Mono<UserResponseDto> signup(UserRequestDto userRequestDto) {
         return userService.signup(userRequestDto)
                 .map(UserResponseDto::new);
+    }
+
+    public Mono<User> findUserByEmail(String email) {
+        return userService.findByEmail(email);
     }
 }
