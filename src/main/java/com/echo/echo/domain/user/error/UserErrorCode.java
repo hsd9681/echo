1fd984f24ba2920ada237e5a78f8119cd0daddc2
kiseokkm <311kskim@gmail.com>
2024-07-30 02:17:57 +0900

    feat: 프로필 조회,수정,비밀번호 변경

--- src/main/java/com/echo/echo/domain/user/error/UserErrorCode.java
+++ src/main/java/com/echo/echo/domain/user/error/UserErrorCode.java
@@ -9,10 +9,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode implements BaseCode {
+
     INCORRECT_VERIFICATION_NUMBER(HttpStatus.BAD_REQUEST, 100, "인증번호가 올바르지 않습니다.", "인증번호가 올바르지 않습니다."),
     USER_NOT_FOUND(HttpStatus.BAD_REQUEST, 101, "해당하는 유저를 찾을 수 없습니다.", "해당하는 이메일을 찾을 수 없습니다."),
     ALREADY_EXIST_EMAIL(HttpStatus.BAD_REQUEST, 102, "이미 존재하는 이메일입니다.", "이미 존재하는 이메일입니다."),
     ALREADY_ACCOUNT_ACTIVATED(HttpStatus.BAD_REQUEST, 103, "이미 인증 완료된 계정입니다.", "이미 활성화 완료된 계정입니다."),
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, 104, "패스워드가 일치하지 않습니다.", "패스워드가 일치하지 않습니다."),
     ;
 
     private final HttpStatus httpStatus;
