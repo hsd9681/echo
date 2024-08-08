a7182f96ddbeaaab2fef379e02e537403725b077
hyun <apfhd0257@naver.com>
2024-08-08 10:00:20 +0900

    fix: health check 추가

--- src/main/java/com/echo/echo/security/config/SecurityConfig.java
+++ src/main/java/com/echo/echo/security/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                         .pathMatchers("/users/change/password/**").permitAll()
                         .pathMatchers(HttpMethod.GET, "/video/**").permitAll()
                         .pathMatchers(HttpMethod.GET, "/text/**").permitAll()
+                        .pathMatchers("/actuator/health").permitAll()
                         .anyExchange().authenticated()
                 )
                 .addFilterAt(authenticationWebFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
